07be1437985578fcfc5f189ee4af144b0ad0d7b4
yas-ako <105139975+yas-ako@users.noreply.github.com>
2026-01-01 17:30:37 +0900

    chore: ダブルクオーテーションを使用するように

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,62 +1,60 @@
-import stylistic from '@stylistic/eslint-plugin';
-import typescriptEslintParser from '@typescript-eslint/parser';
-import astroParser from 'astro-eslint-parser';
-import eslintPluginAstro from 'eslint-plugin-astro';
-import importPlugin from 'eslint-plugin-import';
-import tseslint from 'typescript-eslint';
+import stylistic from "@stylistic/eslint-plugin";
+import typescriptEslintParser from "@typescript-eslint/parser";
+import astroParser from "astro-eslint-parser";
+import eslintPluginAstro from "eslint-plugin-astro";
+import importPlugin from "eslint-plugin-import";
+import tseslint from "typescript-eslint";
 
 export default [
   ...eslintPluginAstro.configs.recommended,
   ...tseslint.configs.recommended,
   
   {
-    files: ['**/*.astro'],
+    files: ["**/*.astro"],
     languageOptions: {
       parser: astroParser,
       parserOptions: {
         parser: typescriptEslintParser,
-        extraFileExtensions: ['.astro'],
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
+        extraFileExtensions: [".astro"],
       },
     },
   },
   {
     plugins: {
-      '@stylistic': stylistic,
+      "@stylistic": stylistic,
       import: importPlugin,
     },
     settings: {
-      'import/resolver': {
+      "import/resolver": {
         typescript: {
           alwaysTryTypes: true,
         },
       },
     },
     rules: {
-      'import/order': [
-        'error',
+      "import/order": [
+        "error",
         {
           groups: [
-            'builtin',
-            'external',
-            'internal',
-            ['parent', 'sibling'],
-            'index',
-            'object',
-            'type',
+            "builtin",
+            "external",
+            "internal",
+            ["parent", "sibling"],
+            "index",
+            "object",
+            "type",
           ],
-          'newlines-between': 'always',
-          pathGroupsExcludedImportTypes: ['builtin'],
-          alphabetize: { order: 'asc', caseInsensitive: true },
+          "newlines-between": "always",
+          pathGroupsExcludedImportTypes: ["builtin"],
+          alphabetize: { order: "asc", caseInsensitive: true },
         },
       ],
-      '@stylistic/indent': ['error', 2],
-      '@stylistic/quotes': ['error', 'single'],
-      '@stylistic/semi': ['error', 'always'],
+      "@stylistic/indent": ["error", 2],
+      "@stylistic/quotes": ["error", "double"],
+      "@stylistic/semi": ["error", "always"],
     }
   },
   {
-    ignores: ['dist', 'node_modules', '.astro']
+    ignores: ["dist", "node_modules", ".astro"]
   }
 ];
